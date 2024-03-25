b6d7a13c79d5d4984134500fced29c58650c3db8
Yonfaaa <3064288605@qq.com>
2024-03-25 23:08:05 +0800

    前后端分离项目 version1

--- springboot/src/main/java/com/example/demo/DemoApplication.java
+++ springboot/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.example.demo.mapper")
 public class DemoApplication {
 
+	//test1
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
