609283e39e82b8afe0f4a2dc99fb7f4c366adc5a
Yonfaaa <3064288605@qq.com>
2024-03-25 23:28:40 +0800

    gyf 测试

--- springboot/src/main/java/com/example/demo/DemoApplication.java
+++ springboot/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	//test by gyf
-	int tyj;
+
 	//test by tyj
 
 	public static void main(String[] args) {
