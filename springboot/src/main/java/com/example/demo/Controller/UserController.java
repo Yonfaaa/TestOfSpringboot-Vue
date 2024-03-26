3971a7cbb1d475863148b10efa913d0383a2105a
Yonfaaa <3064288605@qq.com>
2024-03-26 18:44:17 +0800

    gyf 下篇修改内容(最后一次修改）

--- springboot/src/main/java/com/example/demo/Controller/UserController.java
+++ springboot/src/main/java/com/example/demo/Controller/UserController.java
@@ -32,6 +32,12 @@ public class UserController {
         return Result.success();
     }
 
+    @DeleteMapping("/{id}")
+    public Result<?> update(@PathVariable Long id){
+        userMapper.deleteByID(id);
+        return Result.success();
+    }
+
     @GetMapping
     public Result<?> findPage(@RequestParam(defaultValue = "1") Integer pageNum,
                               @RequestParam(defaultValue = "10") Integer pageSize,
